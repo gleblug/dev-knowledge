bea743e2df77ca5458fa88c45dfaddc009228e37
Gleb Lugovtsov <lugovtsov.gs@phystech.edu>
2024-03-26 14:00:26 +0300

    3rd lesson home task

---
+++ courses/vk-algorithms/03-search-algorithms/05-remove-duplicates.h
@@ -0,0 +1,16 @@
+#include <string>
+
+// stack-like solution
+// copy std::string!
+inline std::string remove_duplicates(const std::string &str)
+{
+    std::string res;
+    for (const char el : str)
+    {
+        if (!res.empty() && (el == res.back()))
+            res.pop_back();
+        else
+            res.push_back(el);
+    }
+    return res;
+}
